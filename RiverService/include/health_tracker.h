f7a025653983199036604dab858b4b21dca8eacb
James McKay <jhmckay93@gmail.com>
2019-04-06 11:43:19 +1300

    added missing file

---
+++ RiverService/include/health_tracker.h
@@ -0,0 +1,37 @@
+#ifndef HEALTH_TRACKER_H
+#define HEALTH_TRACKER_H
+
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+enum application_status {
+    okay,
+    error
+};
+
+class health_tracker {
+    chrono::system_clock::time_point start_time;
+    int http_requests_made = 0;
+    application_status status = okay;
+
+public:
+    health_tracker() {
+        start_time = chrono::system_clock::now();
+    }
+
+    chrono::duration<double> get_uptime() {
+        return chrono::system_clock::now() - start_time;
+    }
+
+    string get_status() {
+        return "Okay";
+    }
+
+
+};
+
+
+#endif
