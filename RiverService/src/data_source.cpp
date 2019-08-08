a1250152e69ffaaea676e7daea141b26a2b88ac9
James McKay <jhmckay93@gmail.com>
2019-08-08 20:28:48 +1200

    added Southland and fixed units

--- RiverService/src/data_source.cpp
+++ RiverService/src/data_source.cpp
@@ -17,7 +17,7 @@ map<utility::string_t, feature_of_interest*> data_source::get_available_features
     for (auto &entry : feature_map) {
         count++;
         //if (count < 10) {
-        //if (entry.second->get_name() == utility::conversions::to_string_t("Waingaromia River at Terrace Station"))
+        //if (entry.second->get_name() == utility::conversions::to_string_t("Hikuwai River at No 4 Bridge"))
         //{
             update_feature(entry.second);
             update_queue.push(entry.second);
